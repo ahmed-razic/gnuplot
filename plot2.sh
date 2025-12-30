d1b18d69fc874b70cbd491ba4449df57bdd9795d
ahmed-razic <ahmed.razic@gmail.com>
2025-12-30 22:43:10 +0100

    numerical calcs finished

--- plot2.sh
+++ plot2.sh
@@ -1,8 +1,22 @@
 #2 - Numerical calculations
+set terminal qt 0
 
-plot "data/data.dat" using 1:2 with lines, \
-"data/data.dat" using 1:3 with lines, \
-"data/data.dat" using 1:4 with lines
+set title "Pade approximation"
+
+set xlabel "x"
+set ylabel "y=exp(-x)"
+
+set xrange [0:2]
+set yrange [0:1]
+
+set xtics 1     #interval [range] increment
+set mxtics 5    #number of interval divisions
+set ytics 0.5
+set mytics 3
+
+plot "data/data.dat" using 1:2 title "Analytical" with lines, "data/data.dat" using 1:3 title "L=1, M=2" with lines, \
+"data/data.dat" using 1:4 title "L=2, M=1" with lines
+
 
 pause -1
 
